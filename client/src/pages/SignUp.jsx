3975880bc26076c9cc982d039646ffed5c225bd2
umairahmed88 <umairahmed_88@ymail.com>
2023-10-19 10:04:17 +0500

    completed sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -74,7 +74,7 @@ export default function SignUp() {
 			</form>
 			<div className='flex gap-2 mt-5'>
 				<p>Already have an account?</p>
-				<Link to='/sing-in'>
+				<Link to='/sign-in'>
 					<span className=' text-blue-700'>Sign In</span>
 				</Link>
 			</div>
