a6b245eb22bc139c24039847bf0b1b89309167a4
umairahmed88 <umairahmed_88@ymail.com>
2023-10-20 13:23:28 +0500

    completed create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 
 export default function App() {
 	return (
@@ -17,6 +18,7 @@ export default function App() {
 				<Route path='/about' element={<SignUp />} />
 				<Route element={<PrivateRoute />}>
 					<Route path='/profile' element={<Profile />} />
+					<Route path='/create-listing' element={<CreateListing />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { app } from "../firebase";
+import { Link } from "react-router-dom";
 import {
 	getDownloadURL,
 	getStorage,
 	ref,
 	uploadBytesResumable,
 } from "firebase/storage";
-import { app } from "../firebase";
 import {
 	deleteUserFailure,
 	deleteUserStart,
@@ -185,6 +186,12 @@ export default function Profile() {
 				>
 					{loading ? "Loading..." : "Update"}
 				</button>
+				<Link
+					className=' bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+					to='/create-listing'
+				>
+					Create Listing
+				</Link>
 			</form>
 			<div className='flex justify-between mt-5'>
 				<span
