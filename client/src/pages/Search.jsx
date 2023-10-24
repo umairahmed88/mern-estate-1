9ea898f985e229f44963a7eb1314c94128f8edb7
umairahmed88 <umairahmed_88@ymail.com>
2023-10-24 10:47:37 +0500

    Created the listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 export default function Search() {
 	const navigate = useNavigate();
@@ -89,7 +90,7 @@ export default function Search() {
 		if (e.target.id === "sort_order") {
 			const sort = e.target.value.split("_")[0] || "created_at";
 
-			const order = e.target.value.split("_") || "desc";
+			const order = e.target.value.split("_")[1] || "desc";
 
 			setSidebardata({ ...sidebardata, sort, order });
 		}
@@ -212,10 +213,25 @@ export default function Search() {
 					</button>
 				</form>
 			</div>
-			<div className=''>
+			<div className=' flex-1'>
 				<h1 className=' text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
 					Listing Results
 				</h1>
+				<div className=' p-7 flex flex-wrap gap-4'>
+					{!loading && listings?.length === 0 && (
+						<p className=' text-xl text-slate-700'>No listing found</p>
+					)}
+					{loading && (
+						<p className=' text-xl text-slate-700 text-center w-full'>
+							Loading...
+						</p>
+					)}
+					{!loading &&
+						listings &&
+						listings.map((listing) => (
+							<ListingItem key={listing._id} listing={listing} />
+						))}
+				</div>
 			</div>
 		</div>
 	);
